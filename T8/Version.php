60ed607fe2a5533335fd76ea63612d5d70214e51
Robbie Perrone <49956805+Robbie320@users.noreply.github.com>
2020-12-14 15:24:26 -0500

    Add files via upload

---
+++ T8/Version.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<!-------------------------
+		Previous Versions
+Version 0.1 Alpha : October 07, 2019
+Version 0.2 Alpha : October 21, 2019 -- Updated SQLTables.php file to include a display tables function
+version 3 Alpha : November 7th, 2019 -- Changed Version Numbers to Whole Numbers, Updated MISC page to Admin Functions page and added forms to add info into all the tables
+as well as display all the explain and information within the tables.
+Version 4 Alpha : November 24th, 2019 -- Redesign, All Forms complete, loggin, update/delete button, validation, near final product
+Version 5 Alpha : December 1st, 2019 -- Added serperate buttons for viewing lost, found, and matched items individually. Added additional validation for email, password, and text box inputs using preg_match. 
+Version 1 Beta : December 4th, 2019 -- Added group photo, added color coding, Changed update button functionality, Fixed cookies.
+
+----------------------------->
+	<p>
+		Version 1 : Updated December 4th, 2019
+		<br></br>
+		Contact Names : Charlie Schmitz, Nicholas Ruiz, Robert Perrone
+		<br></br>
+		<img src= "Group Photo.png" width="70" height ="50">
+	</p>
+
+</html>
